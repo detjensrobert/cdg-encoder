3bf9c88c9559ca09ec73eed8a9a62a3d5a9a011e
Robert Detjens <github@detjens.dev>
2024-03-13 14:04:29 -0700

    update palette gen, write out monitor file for now

--- libcdg/libcdg.py
+++ libcdg/libcdg.py
@@ -23,10 +23,15 @@ class Video:
     log = logging.getLogger("libcdg")
     log.setLevel("DEBUG")
 
-    def __init__(self, source: str | os.PathLike, mono=False, palette: str | os.PathLike=None, quiet=True, fill_frame=False) -> None:
-        """
-
-        """
+    def __init__(
+        self,
+        source: str | os.PathLike,
+        mono=False,
+        palette: str | os.PathLike = None,
+        quiet=True,
+        fill_frame=False,
+    ) -> None:
+        """ """
 
         self.source = str(source)
         self.mono = mono
@@ -44,7 +49,7 @@ class Video:
         "shared scale input video scale ffmpeg pipeline"
 
         # create scale pipeline
-        scaled = (
+        ppl = (
             ffmpeg.input(self.source)
             # ensure 30fps to better match packet rate
             .filter("fps", fps=self.FRAME_RATE)
@@ -52,11 +57,11 @@ class Video:
 
         # scale to CDG canvas size (either display or full)
         if self.fill_frame:
-            scaled = scaled.filter("scale", width=FULL_WIDTH, height=FULL_HEIGHT)
+            ppl = ppl.filter("scale", width=FULL_WIDTH, height=FULL_HEIGHT, flags="neighbor")
         else:
-            scaled = (
-                scaled
-                .filter("scale", width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT)
+            ppl = (
+                ppl.filter("scale", width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, flags="neighbor")
+                # still pad back up to full size so it is centered
                 .filter("pad", width=FULL_WIDTH, height=FULL_HEIGHT, x=-1, y=-1)
             )
 
@@ -64,63 +69,94 @@ class Video:
             dim = f"s={FULL_WIDTH}x{FULL_HEIGHT}"
             black = ffmpeg.input(f"color=Black:{dim}", f="lavfi")
             white = ffmpeg.input(f"color=White:{dim}", f="lavfi")
-            gray = ffmpeg.input( f"color=DarkGray:{dim}", f="lavfi")
+            gray = ffmpeg.input(f"color=DarkGray:{dim}", f="lavfi")
 
-            scaled = ffmpeg.filter([scaled, gray, black, white], "threshold")
+            ppl = ffmpeg.filter([ppl, gray, black, white], "threshold")
 
-        return scaled
+        return ppl
 
     def calc_palette(self, palette_img) -> list[tuple[int, int, int]]:
         "Calculate target palette based on input, or from given image"
 
         if palette_img and self.mono:
-            self.log.warning("both palette image and mono flag give, using palette image!", file=sys.stderr)
+            self.log.warning(
+                "both palette image and mono flag give, using mono palette!",
+                file=sys.stderr,
+            )
 
+        if self.mono:
+            self.log.debug("writing out mono palette")
 
-        if palette_img:
-            self.log.info(f"using palette from {palette_img}")
-            with Image.open(palette_img) as p:
-                assert len(p.getcolors()) <= 16, f"palette file {palette_img} has more than 16 colors!"
+            palette = [(0,0,0), (255,255,255)]
 
-            # use given image as source for ffmpeg palettegen
-            # (ffmpeg wants specific size later so let it generate for itself)
-            palette_input = ffmpeg.input(palette_img)
+            # generate 16x16 palette image internally
+            img = Image.frombytes("L", (16, 16), b"\xFF" + b"\x00"*255)
+
+            # save palette to tempfile for later ffmpeg
+            self.palette_file = tempfile.NamedTemporaryFile(
+                prefix="libcdg_pallette_", suffix=".png", delete=False
+            )
+            img.save(self.palette_file.name)
+
+            return palette
 
         else:
-            if not self.mono:
+            if palette_img:
+                self.log.info(f"using palette from {palette_img}")
+                with Image.open(palette_img) as p:
+                    assert (
+                        len(p.getcolors()) <= 16
+                    ), f"palette file {palette_img} has more than 16 colors!"
+
+                # use given image as source for ffmpeg palettegen
+                # (ffmpeg wants specific size later so let it generate for itself)
+                palette_input = ffmpeg.input(palette_img)
+
+            else:
                 self.log.info(f"deriving palette from input video")
-            # no image, calculate from source
-            palette_input = self.ff_scale_input()
+                # no image, calculate from source
+                palette_input = ffmpeg.input(self.source)
 
+                # crunch source vid or palette image to 16 colors (for global palette)
+                palettegen = palette_input.filter(
+                    "palettegen", max_colors=16, reserve_transparent=0
+                )
+                # TODO remove the other 256-16 colors from output png?
 
-        # crunch source vid or palette image to 16 colors (for global palette)
-        palettegen = palette_input.filter("palettegen", max_colors=16, reserve_transparent=0)
-        # TODO remove the other 256-16 colors from output png?
+                # save palette to tempfile for later use
+                self.palette_file = tempfile.NamedTemporaryFile(
+                    prefix="libcdg_pallette_", suffix=".png"
+                )
+                (
+                    palettegen.output(self.palette_file.name, vframes=1)
+                    .global_args("-hide_banner")
+                    .overwrite_output()
+                    .run(quiet=self.quiet)
+                )
 
-        # save palette to tempfile for later use
-        self.palette_file = tempfile.NamedTemporaryFile(prefix="libcdg_pallette_", suffix=".png")
-        (
-            palettegen.output(self.palette_file.name, vframes=1)
-            .global_args("-hide_banner")
-            .overwrite_output()
-            .run(quiet=self.quiet)
-        )
+            with Image.open(self.palette_file) as palimg:
+                palimg = palimg.convert("P")
 
-        with Image.open(self.palette_file) as palimg:
-            palimg = palimg.convert("P")
+                palette = list(groups_of(palimg.getpalette(), 3))
+                # remove duplicates
+                palette = list(set([tuple(rgb) for rgb in palette]))
 
-            palette = list(groups_of(palimg.getpalette(), 3))
-            # remove duplicates
-            palette = list(set([tuple(rgb) for rgb in palette]))
-
-            assert len(palette) <= 16, "too many colors in palette image!"
-            return palette
+                assert len(palette) <= 16, f"too many colors in palette image! (got {len(palette)})"
+                return palette
 
     def start_ffmpeg(self) -> subprocess.Popen:
         "Captures output frames from ffmpeg over pipe"
 
         # from palette gen
         palette = ffmpeg.input(filename=self.palette_file.name)
+
+        # write out monitor file
+        (
+            ffmpeg.filter([self.ff_scale_input(), palette], "paletteuse")
+            .output(self.source + "_monitor.mp4")
+            .global_args("-hide_banner", "-loglevel", "warning")
+            .run()
+        )
 
         ffprocess = (
             ffmpeg.filter([self.ff_scale_input(), palette], "paletteuse")
@@ -164,7 +200,7 @@ class Video:
                 frame_packets = []
 
                 # fetch the first however-many frames we can fit this round
-                updates_this_frame = list(deltas.queue)[:self.PACKETS_PER_FRAME]
+                updates_this_frame = list(deltas.queue)[: self.PACKETS_PER_FRAME]
                 for _pri, row, col, data in updates_this_frame:
                     # write out instruction packet
                     frame_packets.append(self.write_block(data, row, col))
@@ -178,18 +214,23 @@ class Video:
                 if len(frame_packets) < self.PACKETS_PER_FRAME:
                     # self.log.trace(f"padding extra {self.PACKETS_PER_FRAME - len(frame_packets)}")
 
-                    frame_packets += [instructions.nop()] * (self.PACKETS_PER_FRAME - len(frame_packets))
+                    frame_packets += [instructions.nop()] * (
+                        self.PACKETS_PER_FRAME - len(frame_packets)
+                    )
 
-                assert len(frame_packets) == self.PACKETS_PER_FRAME, f"{len(frame_packets)} is more than {self.PACKETS_PER_FRAME}!"
+                assert (
+                    len(frame_packets) == self.PACKETS_PER_FRAME
+                ), f"{len(frame_packets)} is more than {self.PACKETS_PER_FRAME}!"
 
                 self.packets += frame_packets
 
         return self
 
-
     def save(self, name: str, overwrite=False):
         "Save encoded CDG stream to `name`.cdg and `name`.mp3"
-        assert len(self.packets) != 0, "cannot save before encoding! run `encode()` first"
+        assert (
+            len(self.packets) != 0
+        ), "cannot save before encoding! run `encode()` first"
         self.log.info(f"saving to {name}.cdg/.mp3")
 
         mode = "wb" if overwrite else "xb"
@@ -197,11 +238,12 @@ class Video:
             cdgfile.writelines(self.packets)
 
         # also write out audio
-        mp3 = ffmpeg.input(self.source).output(f"{name}.mp3").global_args("-hide_banner")
+        mp3 = (
+            ffmpeg.input(self.source).output(f"{name}.mp3").global_args("-hide_banner")
+        )
         if overwrite:
             mp3 = mp3.overwrite_output()
         mp3.run(quiet=self.quiet)
-
 
     def image_to_blocks(self, image: Image.Image) -> DisplayFrame | FullFrame:
         "groups `image` pixel data into (numpy) array of block tiles"
@@ -211,8 +253,12 @@ class Video:
         arr = np.array(image)
 
         h, w = arr.shape
-        assert h % BLOCK_HEIGHT == 0, f"{h} rows is not evenly divisible by {BLOCK_HEIGHT}"
-        assert w % BLOCK_WIDTH == 0, f"{w} cols is not evenly divisible by {BLOCK_WIDTH}"
+        assert (
+            h % BLOCK_HEIGHT == 0
+        ), f"{h} rows is not evenly divisible by {BLOCK_HEIGHT}"
+        assert (
+            w % BLOCK_WIDTH == 0
+        ), f"{w} cols is not evenly divisible by {BLOCK_WIDTH}"
 
         # chunked, but one dimensional
         blocks_1d = (
@@ -234,9 +280,11 @@ class Video:
             squashed = (
                 bimg
                 # squish to two arbitrary colors
-                .convert("RGB").quantize(colors=2)
+                .convert("RGB")
+                .quantize(colors=2)
                 # fit back in original palette (no dither to preserve the two colors)
-                .convert("RGB").quantize(palette=image, dither=Image.Dither.NONE)
+                .convert("RGB")
+                .quantize(palette=image, dither=Image.Dither.NONE)
             )
             # (palette index is the same as original)
 
@@ -248,9 +296,11 @@ class Video:
         # split chunk list back into 2d (split into X cols)
         return np.split(blocks_1d, h / BLOCK_HEIGHT)
 
-
     def calc_updates(self, next: Image.Image, prev: Image.Image) -> queue.PriorityQueue:
         "calculate list of blocks to change in order of largest difference"
+
+        # cells need at least this many pixels changed to be tracked
+        PIXEL_THRESHOLD = 4
 
         # array shape: 16x48 x 12x6
         # (blocks in canvas)   (pixels in block)
@@ -264,16 +314,15 @@ class Video:
         # count number of differing pixels
         diff = lambda a, b: len([True for ai, bi in zip(a.flat, b.flat) if ai != bi])
 
-        for row_i, rows in enumerate(np.stack((prev_blocks, next_blocks), 2), start=0):
-            for col_i, (pblock, nblock) in enumerate(rows, start=0):
+        for row_i, rows in enumerate(np.stack((prev_blocks, next_blocks), 2)):
+            for col_i, (pblock, nblock) in enumerate(rows):
                 block_diff = diff(pblock, nblock)
-                if block_diff > 0:
+                if block_diff > PIXEL_THRESHOLD:
                     # negative delta, since PQ fetches lowest values first
                     updates.put((-block_diff, row_i, col_i, nblock))
 
         self.log.debug(f"generated {updates.qsize()} updates")
         return updates
-
 
     def write_block(self, block: Block, row: int, col: int) -> bytes:
         "converts block to fg/bg and returns generated instruction"
@@ -291,7 +340,7 @@ class Video:
         # turn palette index into fg/bg bools
         bools = np.vectorize(lambda x: x == fg)(block)
         # only lower 6 bits are used, so pad to 8 bits for packing
-        padded = np.pad(bools, ((0,0), (2,0)))
+        padded = np.pad(bools, ((0, 0), (2, 0)))
         # pack bools into bitfield
         bits = b"".join(np.packbits(padded))
 
